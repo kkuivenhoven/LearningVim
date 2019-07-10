817b297314e5da94a577511af5ae33cb4af0ecad
Kendra Kuivenhoven <kendrakuivenhoven@Kendras-MacBook-Pro.local>
2019-07-09 18:53:57 -0700

    (bug): fixed new blog post field -- now field condenses when user removes line carriage returns to 0

--- public/assets/application-03fbf3dab9ecc836f7ff0e31e52da7fbfe37851db33e94bcdc54188442de9d0c.js
+++ public/assets/application-03fbf3dab9ecc836f7ff0e31e52da7fbfe37851db33e94bcdc54188442de9d0c.js
@@ -19894,10 +19894,71 @@ Copyright © 2018 Basecamp, LLC
 
 
 
-(function() {
+var new_blog_post_height = 0;
+var new_blog_title_height = 0;
+var cursor_position = 0;
+var title_cursor_position = 0;
+var blog_selection_start_val = 0;
+var blog_scroll_height = 26;
 
 
-}).call(this);
+$(document).on('turbolinks:load', function(){
+	if(window.location.includes("blogs/new")){
+		new_blog_post_height = $('#blog_post').prop('scrollHeight'); 
+		new_blog_title_height = $('#blog_post').prop('scrollHeight'); 
+	}
+});
+
+
+function expand_textarea(){
+	// alert(" reached expand_textarea()");
+}
+
+
+function on_input(obj){
+	if(parseInt($('#blog_post').prop("selectionStart")) == 1){
+		new_blog_post_height = $('#blog_post').prop('scrollHeight');
+		blog_selection_start_val = $('#blog_post').prop('selectionStart');
+	}
+	cursor_position = $('#blog_post').prop("selectionStart");
+	if((cursor_position * blog_scroll_height) > new_blog_post_height){
+		$('#blog_post').height(cursor_position * blog_scroll_height);
+		blog_selection_start_val = $('#blog_post').prop('selectionStart');
+	} 
+	else {
+		if(cursor_position != 0){
+			if(cursor_position != 1){
+				$('#blog_post').height(cursor_position * blog_scroll_height);
+			} else {
+				$('#blog_post').height(52);
+			} 
+		} 
+		else {
+			// $('#blog_post').height((cursor_position+1) * blog_scroll_height);
+			$('#blog_post').height(52);
+		}
+		blog_selection_start_val = $('#blog_post').prop('selectionStart');
+	}
+}
+
+
+function title_on_input(obj){
+	if(parseInt($('#blog_title').prop("selectionStart")) == 1){
+		new_blog_title_height = $('#blog_title').prop('scrollHeight');
+	}
+	title_cursor_position = $('#blog_title').prop("scrollHeight");
+	if(title_cursor_position > new_blog_title_height){
+		$('#blog_title').height(title_cursor_position);
+	}
+}
+
+
+function on_key_stroke(e){
+	// alert(" event.keyCode: ");
+}	
+
+
+;
 (function() {
   (function() {
     (function() {
