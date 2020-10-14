14f8e6745f557b5419986e5e56994eabfd553ddb
kkuivenhoven <kkuivenhoven@mail.csuchico.edu>
2020-10-13 22:18:45 -0700

    set the public file server

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,3 +10,5 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( vim_commands.js)
+
+Rails.application.config.public_file_server.enabled = true
