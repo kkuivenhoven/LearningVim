93cf845273a45731b246bb9c9b86240ac2bd72d3
kkuivenhoven <kkuivenhoven@mail.csuchico.edu>
2020-10-14 23:20:13 -0700

    added basic auth and logout method

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,11 +1,19 @@
+=begin
+	documentation for http basic auth:
+		https://api.rubyonrails.org/classes/ActionController/HttpAuthentication/Basic.html
+=end
+
 class BlogsController < ApplicationController
+	http_basic_authenticate_with name: "dhh", password: "secret", except: :index
 
 	def index
 		@blogs = Blog.all
 	end
 
+=begin
 	def show
 	end
+=end
 
 	def new
 		@blog = Blog.new
@@ -32,6 +40,30 @@ class BlogsController < ApplicationController
 
 	def destroy
 	end
+
+	def logout
+		flash[:alert] = "inside logout action"
+		redirect_to root_path
+		return
+=begin
+		self.current_user.forget_me if logged_in?
+		cookies.delete :auth_token
+		reset_session
+		# redirect_to '/', status: 401, flash: "You have been logged out."
+		# redirect_to '/'#, lash: "You have been logged out."
+		redirect_to root_path #, lash: "You have been logged out."
+		# render :logout, status: 401
+=end
+	end
+
+=begin
+	def destroy 
+		self.current_user.forget_me if logged_in?
+		cookies.delete :auth_token
+		reset_session
+		redirect_to '/', status: 401, flash: "You have been logged out."
+	end
+=end
 
 	private
 		
