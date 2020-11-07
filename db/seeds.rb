58fdd43ad2b4d7816265dbbc655f43b2d23d2ff5
kkuivenhoven <kkuivenhoven@mail.csuchico.edu>
2020-11-07 10:40:50 -0800

    added some more uniform styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Blog.create!(title: "Cupcake Ipsum", post: "Cupcake ipsum dolor sit amet. Chocolate bar donut muffin cupcake tiramisu caramels biscuit oat cake. Cake sesame snaps cheesecake powder chupa chups cotton candy tiramisu tart. Cupcake sweet cookie. Gummi bears marshmallow fruitcake. Cake chocolate bar pastry toffee candy canes pastry chocolate cake. Powder croissant candy chupa chups dragée cotton candy liquorice. Carrot cake cake cotton candy. Soufflé cake sweet roll lemon drops. Candy liquorice candy sesame snaps cookie. Gummies lemon drops fruitcake jujubes tootsie roll danish. Icing chocolate candy bear claw cheesecake dessert apple pie bear claw. Pie muffin cake tootsie roll cheesecake tiramisu caramels.")
+Blog.create!(title: "Cheese Ipsum", post: "Cheesy grin lancashire mozzarella. Bavarian bergkase cheese and wine who moved my cheese taleggio queso chalk and cheese paneer feta. Paneer manchego cheesy feet macaroni cheese cauliflower cheese jarlsberg croque monsieur cheesecake. Cheesy feet cheese and biscuits goat stilton parmesan blue castello fromage frais halloumi. Cheese triangles everyone loves macaroni cheese cheese on toast.")
