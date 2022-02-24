66cb56d2b013ba0133df41f0d143f796edde7796
Kendra Kuivenhoven <kkuivenhoven@mail.csuchico.edu>
2022-02-23 17:56:57 -0800

    Added google console key to routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
 
 	get 'static_pages/login'
 
+  get '/google6fa08eaf7749d52b.html',
+    to: proc { |env| [200, {}, ["google-site-verification: google6fa08eaf7749d52b.html"]] }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
